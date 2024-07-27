d6ddb99c0141e988ebe9540a7e2ea897456096bd
Vihan7301 <vihangupta7301@gmail.com>
2024-07-27 17:43:50 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,32 @@
+// it is monolitic arcitecture
+
+
 //common js syntax -- 
-const express = require ('express')
-//
+const express = require ('express');
+//in the r
 // import express from 'express';
 
-const colors = require('colors')
-const dotenv = require('dotenv').config()
+// require is es5 syntax
+
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+
+const PORT = 3000;
+// port is palatform where the server is launch
+// it can be any 4 digit no.
+
 const app = express();
+//propreties of express is tranfer into app
+
+app.use(express.json());
+// use take 1 parameter
 
 app.get("/", (req, res) => {
-    res.send({
-      message: "Hello World",
-    });
+    res.send("API is running");
   });
   
-  app.listen(PORT, () =>
-    console.log(`Server running on port ${PORT}`.yellow.bold)
-  );
+  app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`.bgBlue.black)
+  });
+
+
